4f6f78dc4571eb308fcb56decccb077c3175fd1a
BarelyBear <kangas.otso@gmail.com>
2024-02-29 17:48:58 +0200

    minor refactor

--- src/main/java/com/example/presto/Editor/Main.java
+++ src/main/java/com/example/presto/Editor/Main.java
@@ -6,103 +6,40 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 public class Main extends Application {
 
     // Stage : represents the interface window between app and os
     Stage initialStage;
-    static int currentColumn = 0;
-    static int currentRow = 0;
+
     //Scene represents a state of the window
     static String filePath;
 
-    static boolean eventCompleted = false;
-
     static StringBuilder text = new StringBuilder();
+
+    //Cursor initial settings
+    static int currentColumn = 0;
+    static int currentRow = 0;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        KeyboardHandler kh = new KeyboardHandler();
-
         initialStage = primaryStage;
-        final FileChooser fileChooser = new FileChooser();
-
-        Label label = new Label("Welcome to Presto");
-        Button newFile = new Button("New File");
-        Button open = new Button("Open File");
-        open.setOnAction(
-                new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent actionEvent) {
-                        File f = fileChooser.showOpenDialog(primaryStage);
-                        if(f != null){
-                            filePath = f.getPath();
-                            System.out.println("Opened file"+filePath);
-                            eventCompleted = true;
-                        }
-                    }
-                }
-        );
-
-        // TODO check if this funcionality can be refactored into it's own class
-        newFile.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-
-                    filePath = null;
-                    eventCompleted = true;
-                    System.out.println("Opening new file");
-                    primaryStage.setHeight(400);
-                    primaryStage.setWidth(400);
-                    // Parameterize from here? Make a different scene in another file maybe, work in progress
-                    VBox newTextArea = new VBox(400);
-                    Label l = new Label(text.toString());
-
-                    initialStage.addEventFilter(MouseEvent.MOUSE_CLICKED,
-                            new EventHandler<MouseEvent>() {
-                                @Override
-                                public void handle(MouseEvent mouseEvent) {
-                                    // Set cursor position, cursor has to point to anything first
-                                    // System.out.println("Clicked text area");
-                                }
-                            });
-                    initialStage.addEventFilter(KeyEvent.KEY_TYPED,
-                            new EventHandler<KeyEvent>() {
-                                @Override
-                                public void handle(KeyEvent keyEvent) {
-
-
-                                    text.append(keyEvent.getCharacter());
-                                    l.setText(text.toString());
-                                }
-                            });
-
-                    newTextArea.getChildren().add(l);
-                    Scene newFileScene = new Scene(newTextArea,400,400);
-                    initialStage.setScene(newFileScene);
-                    initialStage.setTitle("Presto - New file");
-
-            }
-        });
-
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label,open,newFile);
-        Scene initialScene = new Scene(layout1, 200,200);
-
+        Scene onLaunchScene = onLaunchScene();
         // Are called only once or? Callback environment is a learning goal of project
-        initialStage.setScene(initialScene);
+        initialStage.setScene(onLaunchScene);
         initialStage.setTitle("Presto");
         initialStage.show();
-
     }
 
     public static void main(String[] args) throws Exception {
@@ -110,5 +47,96 @@ public class Main extends Application {
         launch();
     }
 
+    public Scene newFileScene(){
+
+        Pane root = new Pane();
+        VBox wrapper = new VBox(600);
+        Scene fileEditScene = new Scene(root, 600,600);
+        Label l = new Label(text.toString());
+        System.out.println("Label "+text.toString());
+        VBox mainTextArea = new VBox(400);
+
+        /*
+
+        Obviously only one of these should handle the normal appending and inserting situtation, while the other
+        should handle special cases. however, it's more cumbersome as per this method gets the keycodes while
+        the latter does not
+
+         */
+
+
+        fileEditScene.setOnKeyPressed(event -> {
+            // This is apparently good train of thought for handling special characters
+            System.out.println(event.getCode());
+            if(event.getCode() == KeyCode.ENTER){
+                System.out.println("New line");
+            }
+            if(event.getCode() == KeyCode.DELETE){
+                System.out.println("Handle delete");
+                text.delete(text.length(),text.length()-1);
+            }
+
+            // the normal case, maybe handle this more eloquently in the data structure class?
+           text.append(event.getCharacter());
+            l.setText(text.toString());
+        });
+
+
+        // Handle special keys here?
+        root.addEventFilter(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+
+                System.out.println(keyEvent.getCharacter());
+
+                if(keyEvent.getCharacter().equals("ENTER")){
+                    System.out.println("New line");
+                }
+                if(keyEvent.getCharacter().equals("SPACE")){
+                    System.out.println("Leave some space");
+                }
+
+            }
+        });
+
+        mainTextArea.getChildren().add(l);
+        wrapper.getChildren().add(mainTextArea);
+        root.getChildren().add(wrapper);
+
+        return fileEditScene;
+    }
+
+    public Scene onLaunchScene(){
+
+        final FileChooser fileChooser = new FileChooser();
+        Pane root = new Pane();
+        VBox wrapper = new VBox(20);
+        Label label = new Label("Welcome to Presto");
+        Button newFileButton = new Button("New File");
+        Button openFileButton = new Button("Open File");
+        openFileButton.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        File f = fileChooser.showOpenDialog(initialStage);
+                        if(f != null){
+                            filePath = f.getPath();
+                            System.out.println("Opened file "+filePath);
+                        }
+                    }
+                });
+        newFileButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // initialize newFileScene
+                filePath = null;
+                initialStage.setScene(newFileScene());
+                initialStage.setTitle("Presto - New file");
+            }
+        });
+        wrapper.getChildren().addAll(label,openFileButton,newFileButton);
+        root.getChildren().add(wrapper);
+        return new Scene(root,250,250);
+    }
 
 }
